5a4acd597e5c8cfc72678c2e9e5e04a256c40be8
nicola nassour <nassournikolla@gmail.com>
2021-10-19 10:27:38 +0200

    Added Old Versions Of SQL + Abfragen.pdf

---
+++ MySQL-Doku/3.sql
@@ -0,0 +1,118 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server Version:               10.4.21-MariaDB - mariadb.org binary distribution
+-- Server Betriebssystem:        Win64
+-- HeidiSQL Version:             11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Exportiere Daten aus Tabelle krautundrueben.bestellung: ~12 rows (ungefähr)
+DELETE FROM `bestellung`;
+/*!40000 ALTER TABLE `bestellung` DISABLE KEYS */;
+INSERT INTO `bestellung` (`BESTELLNR`, `KUNDENNR`, `BESTELLDATUM`, `RECHNUNGSBETRAG`) VALUES
+	(1, 2001, '2020-07-01', 6.21),
+	(2, 2002, '2020-07-08', 32.96),
+	(3, 2003, '2020-08-01', 24.08),
+	(4, 2004, '2020-08-02', 19.90),
+	(5, 2005, '2020-08-02', 6.47),
+	(6, 2006, '2020-08-10', 6.96),
+	(7, 2007, '2020-08-10', 2.41),
+	(8, 2008, '2020-08-10', 13.80),
+	(9, 2009, '2020-08-10', 8.67),
+	(10, 2007, '2020-08-15', 17.98),
+	(11, 2005, '2020-08-12', 8.67),
+	(12, 2003, '2020-08-13', 20.87);
+/*!40000 ALTER TABLE `bestellung` ENABLE KEYS */;
+
+-- Exportiere Daten aus Tabelle krautundrueben.bestellungzutat: ~26 rows (ungefähr)
+DELETE FROM `bestellungzutat`;
+/*!40000 ALTER TABLE `bestellungzutat` DISABLE KEYS */;
+INSERT INTO `bestellungzutat` (`BESTELLNR`, `ZUTATENNR`, `MENGE`) VALUES
+	(1, 1001, 5),
+	(1, 1002, 3),
+	(1, 1004, 3),
+	(1, 1006, 2),
+	(2, 1003, 4),
+	(2, 1005, 5),
+	(2, 6408, 5),
+	(2, 9001, 10),
+	(3, 3001, 5),
+	(3, 6300, 15),
+	(4, 3003, 2),
+	(4, 5001, 7),
+	(5, 1001, 5),
+	(5, 1002, 4),
+	(5, 1004, 5),
+	(6, 1010, 5),
+	(7, 1009, 9),
+	(8, 1008, 7),
+	(8, 1012, 5),
+	(9, 1007, 4),
+	(9, 1012, 5),
+	(10, 1011, 7),
+	(10, 4001, 7),
+	(11, 1012, 5),
+	(11, 5001, 2),
+	(12, 1010, 15);
+/*!40000 ALTER TABLE `bestellungzutat` ENABLE KEYS */;
+
+-- Exportiere Daten aus Tabelle krautundrueben.kunde: ~9 rows (ungefähr)
+DELETE FROM `kunde`;
+/*!40000 ALTER TABLE `kunde` DISABLE KEYS */;
+INSERT INTO `kunde` (`KUNDENNR`, `NACHNAME`, `VORNAME`, `GEBURTSDATUM`, `STRASSE`, `HAUSNR`, `PLZ`, `ORT`, `TELEFON`, `EMAIL`) VALUES
+	(2001, 'Wellensteyn', 'Kira', '1990-05-05', 'Eppendorfer Landstrasse', '104', '20249', 'Hamburg', '040/443322', 'k.wellensteyn@yahoo.de'),
+	(2002, 'Foede', 'Dorothea', '2000-03-24', 'Ohmstraße', '23', '22765', 'Hamburg', '040/543822', 'd.foede@web.de'),
+	(2003, 'Leberer', 'Sigrid', '1989-09-21', 'Bilser Berg', '6', '20459', 'Hamburg', '0175/1234588', 'sigrid@leberer.de'),
+	(2004, 'Soerensen', 'Hanna', '1974-04-03', 'Alter Teichweg', '95', '22049', 'Hamburg', '040/634578', 'h.soerensen@yahoo.de'),
+	(2005, 'Schnitter', 'Marten', '1964-04-17', 'Stübels', '10', '22835', 'Barsbüttel', '0176/447587', 'schni_mart@gmail.com'),
+	(2006, 'Maurer', 'Belinda', '1978-09-09', 'Grotelertwiete', '4a', '21075', 'Hamburg', '040/332189', 'belinda1978@yahoo.de'),
+	(2007, 'Gessert', 'Armin', '1978-01-29', 'Küstersweg', '3', '21079', 'Hamburg', '040/67890', 'armin@gessert.de'),
+	(2008, 'Haessig', 'Jean-Marc', '1982-08-30', 'Neugrabener Bahnhofstraße', '30', '21149', 'Hamburg', '0178-67013390', 'jm@haessig.de'),
+	(2009, 'Urocki', 'Eric', '1999-12-04', 'Elbchaussee', '228', '22605', 'Hamburg', '0152-96701390', 'urocki@outlook.de');
+/*!40000 ALTER TABLE `kunde` ENABLE KEYS */;
+
+-- Exportiere Daten aus Tabelle krautundrueben.lieferant: ~3 rows (ungefähr)
+DELETE FROM `lieferant`;
+/*!40000 ALTER TABLE `lieferant` DISABLE KEYS */;
+INSERT INTO `lieferant` (`LIEFERANTENNR`, `LIEFERANTENNAME`, `STRASSE`, `HAUSNR`, `PLZ`, `ORT`, `TELEFON`, `EMAIL`) VALUES
+	(101, 'Bio-Hof Müller', 'Dorfstraße', '74', '24354', 'Weseby', '04354-9080', 'mueller@biohof.de'),
+	(102, 'Obst-Hof Altes Land', 'Westerjork 74', '76', '21635', 'Jork', '04162-4523', 'info@biohof-altesland.de'),
+	(103, 'Molkerei Henning', 'Molkereiwegkundekunde', '13', '19217', 'Dechow', '038873-8976', 'info@molkerei-henning.de');
+/*!40000 ALTER TABLE `lieferant` ENABLE KEYS */;
+
+-- Exportiere Daten aus Tabelle krautundrueben.rezepte: ~5 rows (ungefähr)
+DELETE FROM `rezepte`;
+/*!40000 ALTER TABLE `rezepte` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rezepte` ENABLE KEYS */;
+
+-- Exportiere Daten aus Tabelle krautundrueben.rezepte_alt: ~5 rows (ungefähr)
+DELETE FROM `rezepte_alt`;
+/*!40000 ALTER TABLE `rezepte_alt` DISABLE KEYS */;
+INSERT INTO `rezepte_alt` (`Rezept_NR`, `BEZEICHNUNG`, `Zucchini`, `Zwiebel`, `Tomate`, `Schalotte`, `Karotte`, `Kartoffel`, `Rucola`, `Lauch`, `Knoblauch`, `Basilikum`, `Suesskartoffel`, `Schnittlauch`, `Apfel`, `Vollmilch`, `Mozzarella`, `Butter`, `Ei`, `Wiener_Wuerstchen`, `Kichererbsen`, `Couscous`, `Gemuesebruehe`, `Tofu_Wuerstchen`, `Lammkotelett`, `NETTOPREIS`, `KALORIEN`, `KOHLENHYDRATE`, `PROTEIN`) VALUES
+	(10001, 'English Breakfast', NULL, 1, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, NULL, NULL, NULL, 1, 3, 1, NULL, NULL, NULL, NULL, NULL, 7.50, 831, 14.40, 51.00),
+	(10002, 'Ofen Gemuese', NULL, NULL, NULL, NULL, 3, 3, NULL, NULL, 1, NULL, 3, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 4.60, 563, 162.20, 19.50),
+	(10003, 'Tomate Mozzarella', NULL, NULL, 3, NULL, NULL, NULL, NULL, NULL, NULL, 1, NULL, NULL, NULL, NULL, 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 9.65, 577, 14.90, 42.30),
+	(10004, 'Bunter Salat', NULL, NULL, 1, NULL, 1, NULL, 1, NULL, NULL, NULL, NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, NULL, NULL, NULL, 4.45, 465, 82.70, 19.50),
+	(10005, 'Orientalische Lammkoteletts', NULL, NULL, NULL, 2, NULL, NULL, NULL, NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, 1, NULL, NULL, 1, 1, NULL, NULL, 2, 14.55, 1833, 123.80, 80.60);
+/*!40000 ALTER TABLE `rezepte_alt` ENABLE KEYS */;
+
+-- Exportiere Daten aus Tabelle krautundrueben.rezept_zutaten: ~0 rows (ungefähr)
+DELETE FROM `rezept_zutaten`;
+/*!40000 ALTER TABLE `rezept_zutaten` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rezept_zutaten` ENABLE KEYS */;
+
+-- Exportiere Daten aus Tabelle krautundrueben.zutat: ~0 rows (ungefähr)
+DELETE FROM `zutat`;
+/*!40000 ALTER TABLE `zutat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `zutat` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
